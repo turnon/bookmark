ad93189495ede3bcdc184b298bab57cd2a48f5ba
ken <block24block@gmail.com>
2021-06-01 23:46:28 +0800

    all stat method

--- bookmark/statisitic.go
+++ bookmark/statisitic.go
@@ -7,26 +7,49 @@ type Stat struct {
 	Entries []Entry
 }
 
+type Stats struct {
+	Name   string
+	Label  string
+	Groups []Stat
+}
+
 type statMethod struct {
+	name    string
 	groupBy func(e *Entry) string
 	onlyDup bool
 }
 
 var statMethods = map[string]*statMethod{
 	"dupName": {
+		name:    "名字重复",
 		groupBy: func(e *Entry) string { return e.Name },
 		onlyDup: true,
 	},
 	"dupURL": {
+		name:    "URL重复",
 		groupBy: func(e *Entry) string { return e.URL },
 		onlyDup: true,
 	},
 	"hosts": {
+		name:    "网站统计",
 		groupBy: func(e *Entry) string { return e.Host() },
 	},
 	"folders": {
+		name:    "目录统计",
 		groupBy: func(e *Entry) string { return e.Folder() },
 	},
+}
+
+func (b *Bookmark) Stats() ([]Stats, error) {
+	result := make([]Stats, 0, len(statMethods))
+	for methodName, method := range statMethods {
+		stats, err := b.Stat(methodName)
+		if err != nil {
+			return nil, err
+		}
+		result = append(result, Stats{Name: methodName, Label: method.name, Groups: stats})
+	}
+	return result, nil
 }
 
 func (b *Bookmark) Stat(method string) ([]Stat, error) {
