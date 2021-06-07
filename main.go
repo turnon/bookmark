7d5e0ef3fa2c22522037834ff73def55d219f5bf
ken <block24block@gmail.com>
2021-06-07 22:29:55 +0800

    select stat method

--- main.go
+++ main.go
@@ -9,8 +9,14 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type Query struct {
+type query struct {
 	Stat string `form:"stat"`
+}
+
+type page struct {
+	Stats    *bookmark.Stats
+	StatOpts map[string]string
+	Query    query
 }
 
 func main() {
@@ -26,14 +32,16 @@ func main() {
 
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
-		q := Query{}
+		q := query{}
 		c.ShouldBindQuery(&q)
 		if q.Stat == "" {
 			q.Stat = "dirs"
 		}
-		stats, _ := b.VerboseStat(q.Stat)
 
-		bytes, _ := views.Render("index.html", stats)
+		stats, _ := b.VerboseStat(q.Stat)
+		p := page{Stats: stats, StatOpts: bookmark.StatOpts(), Query: q}
+
+		bytes, _ := views.Render("index.html", p)
 		c.Data(200, "text/html; charset=utf-8", bytes)
 	})
 	r.Run()
